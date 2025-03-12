8c47264aa86366588cf33edf48e2e354e2543b57
RohithKumar2002-05 <skewrohith888@gmail.com>
2025-03-12 14:49:16 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import prisma from '../../lib/prisma';
+import Link from 'next/link';
+export default async function Home() {
 
-export default function Home() {
+const posts = await prisma.post.findMany({})
   return (
     <div className="flex items-center justify-center">
 
+      {posts.length >0 ?(
+        
+        posts.map((post)=>
+         
+          (
+          <div key={post.id} className='border-b max-w flex items-center'>
+              <Link href={`/${post.id}`} className='text-blue'>        <p>{post.title}</p></Link>
+           
+            <p>{post.content}</p>
+          
 
-      Project Deployed 
+
+          </div>
+        ))    ) : <p className='text-slate-500'>No posts available</p>  }
+   
     </div>
   );
 }
